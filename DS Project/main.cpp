82aedc94939d2060f676f8f60a8030c04a1691b2
0xbazooka <2021170119@cis.asu.edu.eg>
2023-05-02 10:29:14 +0300

    options in main added

--- DS Project/main.cpp
+++ DS Project/main.cpp
@@ -8,25 +8,51 @@
 using namespace std;
 
 int main() {
-	
+
+
 	int choice;
+	vector <User> users;
+
+	cout << "to login press 1\nto register press 2\n";
+	cin >> choice;
+	switch (choice) {
+	case 1:User.Login();
+		break;
+	case 2:User.Register();
+		break;
+	}
+	 
 
 	while (true) {
-		cout << "Enter your choice" << endl;
-		case(choice)
-			1:
+		cout << "Choose one of the following options:" << endl;
+		cout << "1. send a message " << endl;
+		cout << "2. undo a sent message" << endl;
+		cout << "3. add a new contact" << endl;
+		cout << "4. remove a contact" << endl;
+		cout << "5. view all contacts" << endl; //sorted by no. of msgs
+		cout << "6. search for a contact" << endl;
+		cout << "7. view all the messages you sent" << endl;
+		cout << "8. view your received messages" << endl;
+		cout << "9. add a message to favourites " << endl;
+		cout << "10. rm the oldest fav msg"<< endl;
+		cout << "11. view your fav messages " << endl;
+		cout << "12. exit " << endl;
+
+
+		switch (choice) {
+			case 1:
+				//User::sendMessage();
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+
+		}
 	}
 
-	User user;
-
-	//cout << "TO LOGIN PRESS 1\nTO REGISTER PRESS 2\n";
-	//cin >> choice;
-	//switch (choice) {
-	//case 1:user.Login();
-	//	break;
-	//case 2:user.Register();
-	//	break;
-	//}
 
 	//return 0;
 }
